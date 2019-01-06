ec9080dc0f9e48af7691ba5628f6085dab945e3c
Mathias Oksen Vanner <Mathias.vanner@live.dk>
2019-01-06 11:23:35 +0100

    Fed

--- Entities/Migrations/20190106102142_MigrationTimestamp.Designer.cs
+++ Entities/Migrations/20190106102142_MigrationTimestamp.Designer.cs
@@ -4,14 +4,16 @@ using Entities.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Entities.Migrations
 {
     [DbContext(typeof(GiftDbContext))]
-    partial class GiftDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190106102142_MigrationTimestamp")]
+    partial class MigrationTimestamp
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
